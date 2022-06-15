5a76fdf1ec80b929b78b3c58e27623050ae04928
Tewodros <tyimers@gmail.com>
2022-06-15 10:40:21 -0400

    created index route and controller for movies and connected to DB

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+require('./config/database')
 
-//
+// ===== ROUTES =====
+// Movies
+app.use('/api/v1/movies', require('./routes/api/movies.js'))
 
 // ===== PORT =====
 const port = 8080
 
 app.listen(port, () => console.log(`Express app running on port ${port}`))
-
-//
